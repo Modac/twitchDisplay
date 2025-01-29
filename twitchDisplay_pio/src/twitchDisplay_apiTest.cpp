fe85572ee53d90ad475e8e5f3f8ef4ccf3898937
Pascal Goldbrunner <modac@gmx.net>
2025-01-29 15:29:06 +0100

    Init pio project

---
+++ twitchDisplay_pio/src/twitchDisplay_apiTest.cpp
@@ -0,0 +1,189 @@
+/**
+ *  TODO:
+ *  - Handle Twitch Websocket msgs (https://dev.twitch.tv/docs/eventsub/handling-websocket-events/)
+ *    - Welcome message
+ *    - Keepalive message
+ *    - Ping message (handled by websockets lib)
+ *    - Reconnect message
+ *    - Revocation message
+ *    - Close message (handled by websockets lib)
+ */
+ 
+#include "secrets.h"
+
+#include <Arduino.h>
+
+#include <WiFi.h>
+#include <WiFiMulti.h>
+#include <WiFiClientSecure.h>
+
+#include <HTTPClient.h>
+#include <WebSocketsClient.h>
+
+#include <ArduinoJson.h>
+
+#define USE_SERIAL Serial
+
+WiFiMulti wifiMulti;
+WebSocketsClient webSocket;
+
+void hexdump(const void *mem, uint32_t len, uint8_t cols = 16) {
+  const uint8_t* src = (const uint8_t*) mem;
+  USE_SERIAL.printf("\n[HEXDUMP] Address: 0x%08X len: 0x%X (%d)", (ptrdiff_t)src, len, len);
+  for(uint32_t i = 0; i < len; i++) {
+    if(i % cols == 0) {
+      USE_SERIAL.printf("\n[0x%08X] 0x%08X: ", (ptrdiff_t)src, i);
+    }
+    USE_SERIAL.printf("%02X ", *src);
+    src++;
+  }
+  USE_SERIAL.printf("\n");
+}
+
+void webSocketEvent(WStype_t type, uint8_t * payload, size_t length) {
+
+  switch(type) {
+    case WStype_DISCONNECTED:
+      USE_SERIAL.printf("[WSc] Disconnected! (length: %u)\n", length);
+      break;
+    case WStype_CONNECTED:
+      USE_SERIAL.printf("[WSc] Connected to url: %s\n", payload);
+
+      // send message to server when Connected
+      //webSocket.sendTXT("Connected");
+      break;
+    case WStype_TEXT:
+      {
+      USE_SERIAL.printf("[WSc] get text: %s\n", payload);
+      
+      // Deserialize the JSON document
+      JsonDocument doc;
+      DeserializationError error = deserializeJson(doc, payload);
+
+      // Test if parsing succeeds.
+      if (error) {
+        USE_SERIAL.print(F("deserializeJson() failed: "));
+        USE_SERIAL.println(error.f_str());
+        return;
+      }
+
+      const char* message_type = doc["metadata"]["message_type"];
+
+      USE_SERIAL.printf("[TwitchApi] got message: %s\n", message_type);
+
+      if(strcmp_P(message_type,(PGM_P) F("session_welcome"))==0){
+        const char* session_id = doc["payload"]["session"]["id"];
+        USE_SERIAL.printf("[TwitchApi] session id: %s\n", session_id);
+      }
+      
+      // send message to server
+      // webSocket.sendTXT("message here");
+      }
+      break;
+    case WStype_BIN:
+      USE_SERIAL.printf("[WSc] get binary length: %u\n", length);
+      hexdump(payload, length);
+
+      // send data to server
+      // webSocket.sendBIN(payload, length);
+      break;
+    case WStype_ERROR:      
+    case WStype_FRAGMENT_TEXT_START:
+    case WStype_FRAGMENT_BIN_START:
+    case WStype_FRAGMENT:
+    case WStype_FRAGMENT_FIN:
+      break;
+  }
+
+}
+
+void twitchCheckOnlineChannels();
+
+void setup() {
+
+  USE_SERIAL.begin(115200);
+
+  USE_SERIAL.setDebugOutput(true);
+  
+  USE_SERIAL.println();
+  USE_SERIAL.println();
+  USE_SERIAL.println();
+
+  for (uint8_t t = 4; t > 0; t--) {
+    USE_SERIAL.printf("[SETUP] WAIT %d...\n", t);
+    USE_SERIAL.flush();
+    delay(1000);
+  }
+
+  wifiMulti.addAP("::1", WIFI_PW);
+  
+  // wait for WiFi connection
+  while((wifiMulti.run() != WL_CONNECTED)){
+    delay(10);
+  }
+  twitchCheckOnlineChannels();
+
+  delay(1000);
+  
+  webSocket.beginSSL("eventsub.wss.twitch.tv", 443, "/ws");
+  // webSocket.begin("192.168.6.61", 1234, "/");
+  
+  // event handler
+  webSocket.onEvent(webSocketEvent);
+
+  // try ever 5000 again if connection has failed
+  webSocket.setReconnectInterval(10000);
+}
+
+void loop() {
+  webSocket.loop();
+}
+
+void twitchCheckOnlineChannels(){
+  HTTPClient http;
+
+    USE_SERIAL.print("[HTTP] begin...\n");
+    http.useHTTP10(true);
+    http.begin("https://api.twitch.tv/helix/streams?user_login=gronkhtv&user_login=gronkh&user_login=pietsmiet");  //HTTP
+
+    USE_SERIAL.print("[HTTP] adding Headers...\n");
+    // add headers
+    http.setAuthorizationType("Bearer");
+    http.setAuthorization(TWITCH_TOKEN);
+    http.addHeader("Client-Id", "gp762nuuoqcoxypju8c569th9wz7q5");
+    
+    USE_SERIAL.print("[HTTP] GET...\n");
+    // start connection and send HTTP header
+    int httpCode = http.GET();
+
+    // httpCode will be negative on error
+    if (httpCode > 0) {
+      // HTTP header has been send and Server response header has been handled
+      USE_SERIAL.printf("[HTTP] GET... code: %d\n", httpCode);
+
+      // file found at server
+      if (httpCode == HTTP_CODE_OK) {
+        //String payload = http.getString();
+        //USE_SERIAL.println(payload);
+        
+        JsonDocument doc;
+        DeserializationError err = deserializeJson(doc, http.getStream());
+        if (err) {
+          Serial.print(F("deserializeJson() failed with code "));
+          Serial.println(err.f_str());
+        } else {
+          for (JsonObject channel : doc["data"].as<JsonArray>()) {
+            const char* name = channel["user_name"];
+            USE_SERIAL.print("Live channel: ");
+            USE_SERIAL.println(name);
+            // etc.
+          }
+        }
+        
+      }
+    } else {
+      USE_SERIAL.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+
+    http.end();
+}
---
+++ twitchDisplay_pio/src/twitchDisplay_displayTest.cpp
@@ -0,0 +1,479 @@
+/**************************************************************************
+  This is a library for several Adafruit displays based on ST77* drivers.
+
+  This example works with the 1.14" TFT breakout
+    ----> https://www.adafruit.com/product/4383
+  The 1.3" TFT breakout
+    ----> https://www.adafruit.com/product/4313
+  The 1.47" TFT breakout
+    ----> https://www.adafruit.com/product/5393
+  The 1.54" TFT breakout
+    ----> https://www.adafruit.com/product/3787
+  The 1.69" TFT breakout
+    ----> https://www.adafruit.com/product/5206
+  The 1.9" TFT breakout
+    ----> https://www.adafruit.com/product/5394
+  The 2.0" TFT breakout
+    ----> https://www.adafruit.com/product/4311
+
+
+  Check out the links above for our tutorials and wiring diagrams.
+  These displays use SPI to communicate, 4 or 5 pins are required to
+  interface (RST is optional).
+
+  Adafruit invests time and resources providing this open source code,
+  please support Adafruit and open-source hardware by purchasing
+  products from Adafruit!
+
+  Written by Limor Fried/Ladyada for Adafruit Industries.
+  MIT license, all text above must be included in any redistribution
+ **************************************************************************/
+
+
+#include <Arduino.h>
+
+#include <pins_arduino.h>
+
+#include <Adafruit_GFX.h>    // Core graphics library
+#include <Adafruit_ST7789.h> // Hardware-specific library for ST7789
+#include <SPI.h>
+#include "pics.h"
+#include "secrets.h"
+#include "twitchDisplay_displayTest.h"
+
+#if defined(ARDUINO_FEATHER_ESP32) // Feather Huzzah32
+  #define TFT_CS         14
+  #define TFT_RST        15
+  #define TFT_DC         32
+
+#elif defined(ESP8266)
+  #define TFT_CS         4
+  #define TFT_RST        16
+  #define TFT_DC         5
+
+#else
+  // For the breakout board, you can use any 2 or 3 pins.
+  // These pins will also work for the 1.8" TFT shield.
+  #define TFT_CS         7
+  #define TFT_RST       10 // Or set to -1 and connect to Arduino RESET pin
+  #define TFT_DC         1
+  #define TFT_BK         0
+#endif
+
+// OPTION 1 (recommended) is to use the HARDWARE SPI pins, which are unique
+// to each board and not reassignable. For Arduino Uno: MOSI = pin 11 and
+// SCLK = pin 13. This is the fastest mode of operation and is required if
+// using the breakout board's microSD card.
+
+Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_RST);
+
+// OPTION 2 lets you interface the display using ANY TWO or THREE PINS,
+// tradeoff being that performance is not as fast as hardware SPI above.
+//#define TFT_MOSI 11  // Data out
+//#define TFT_SCLK 13  // Clock out
+
+//Adafruit_ST7789 tft = Adafruit_ST7789(TFT_CS, TFT_DC, TFT_MOSI, TFT_SCLK, TFT_RST);
+
+float p = 3.1415926;
+
+
+GFXcanvas16 canvas(320, 85); // 16-bit, 320x170 pixels
+GFXcanvas16 text_canvas(298+(6*8), 64); 
+
+void setHighFR();
+void set60FR();
+void enterPartialMode();
+void enterNormalMode();
+void drawGrayscaleBitmapSection(int16_t x, int16_t y, uint8_t *bitmap,
+                                int16_t o_x, int16_t o_y, int16_t w,
+                                int16_t h, int16_t b_w);
+void drawRGBBitmapSectionFast(int16_t x, int16_t y, uint16_t *bitmap,
+                              int16_t o_x, int16_t o_y, int16_t w,
+                              int16_t h, int16_t b_w);
+                              
+void testroundrects();
+void testtriangles();
+
+
+void setup(void) {
+  Serial.begin(9600);
+  Serial.print(F("Hello! ST77xx TFT Test"));
+
+  pinMode(TFT_BK, OUTPUT);
+  digitalWrite(TFT_BK, HIGH);
+
+  // Use this initializer (uncomment) if using a 1.3" or 1.54" 240x240 TFT:
+  //tft.init(240, 240);           // Init ST7789 240x240
+
+  // OR use this initializer (uncomment) if using a 1.69" 280x240 TFT:
+  //tft.init(240, 280);           // Init ST7789 280x240
+
+  // OR use this initializer (uncomment) if using a 2.0" 320x240 TFT:
+  //tft.init(240, 320);           // Init ST7789 320x240
+
+  // OR use this initializer (uncomment) if using a 1.14" 240x135 TFT:
+  //tft.init(135, 240);           // Init ST7789 240x135
+  
+  // OR use this initializer (uncomment) if using a 1.47" 172x320 TFT:
+  //tft.init(172, 320);           // Init ST7789 172x320
+
+  // OR use this initializer (uncomment) if using a 1.9" 170x320 TFT:
+  tft.init(170, 320);           // Init ST7789 170x320
+  tft.setRotation(3);
+
+  // SPI speed defaults to SPI_DEFAULT_FREQ defined in the library, you can override it here
+  // Note that speed allowable depends on chip and quality of wiring, if you go too fast, you
+  // may end up with a black screen some times, or all the time.
+  tft.setSPISpeed(80000000);
+  Serial.println(F("Initialized"));
+
+  uint16_t time = millis();
+  tft.fillScreen(ST77XX_BLACK);
+  time = millis() - time;
+
+  Serial.println(time, DEC);
+  delay(500);
+/*
+  // large block of text
+  tft.fillScreen(ST77XX_BLACK);
+  testdrawtext("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur adipiscing ante sed nibh tincidunt feugiat. Maecenas enim massa, fringilla sed malesuada et, malesuada sit amet turpis. Sed porttitor neque ut ante pretium vitae malesuada nunc bibendum. Nullam aliquet ultrices massa eu hendrerit. Ut sed nisi lorem. In vestibulum purus a tortor imperdiet posuere. ", ST77XX_WHITE);
+  delay(1000);
+
+  // tft print function!
+  tftPrintTest();
+  delay(4000);
+
+  // a single pixel
+  tft.drawPixel(tft.width()/2, tft.height()/2, ST77XX_GREEN);
+  delay(500);
+
+  // line draw test
+  testlines(ST77XX_YELLOW);
+  delay(500);
+
+  // optimized lines
+  testfastlines(ST77XX_RED, ST77XX_BLUE);
+  delay(500);
+
+  testdrawrects(ST77XX_GREEN);
+  delay(500);
+
+  testfillrects(ST77XX_YELLOW, ST77XX_MAGENTA);
+  delay(500);
+
+  tft.fillScreen(ST77XX_BLACK);
+  testfillcircles(10, ST77XX_BLUE);
+  testdrawcircles(10, ST77XX_WHITE);
+  delay(500);
+*/
+
+  testroundrects();
+  delay(500);
+
+  testtriangles();
+  delay(500);
+
+  //mediabuttons();
+  //delay(500);
+
+  tft.fillScreen(ST77XX_BLACK);
+  
+  canvas.fillScreen(ST77XX_BLACK);
+  canvas.drawRGBBitmap(11, 14, epd_bitmap_pietsmiet, 64, 64);
+  tft.drawRGBBitmap(0, 0, canvas.getBuffer(), canvas.width(), canvas.height());
+
+  text_canvas.setCursor(0,0);
+  text_canvas.setTextWrap(false);
+  text_canvas.setTextColor(ST77XX_CYAN);
+  text_canvas.setTextSize(8);
+  
+  setHighFR();
+
+  Serial.println("done");
+  delay(1000);
+}
+
+uint8_t o_X = 0;
+
+char text[] = "Hallo Test wasgehthierab?";
+uint8_t text_offset = 0;
+
+void loop() {
+  if(!o_X){
+    text_canvas.fillScreen(ST77XX_BLACK);
+    text_canvas.setCursor(0,0);
+    text_canvas.print(&text[text_offset]);
+    text_offset++;
+    text_offset%=strlen(text)-5;
+  }
+  //enterPartialMode();
+  tft.fillRect(11, 14+64+14, 298, 64, ST77XX_BLACK);
+  drawRGBBitmapSectionFast(11, 14+64+14, text_canvas.getBuffer(), o_X, 0, 298, text_canvas.height(), text_canvas.width());
+  //enterNormalMode();
+  o_X = (o_X+6)%(6*8);
+  Serial.println(o_X);
+  
+  //tft.invertDisplay(true);
+  if(o_X==0){
+  canvas.fillRect(11+64+14+64+14, 14, 64, 64, ST77XX_BLACK);
+  canvas.fillRect(11+64+14, 14, 64, 64, ST77XX_BLACK);
+  tft.drawRGBBitmap(0, 0, canvas.getBuffer(), canvas.width(), canvas.height());
+  }
+  delay(20);
+  //tft.invertDisplay(false);
+  if(o_X==24){
+  canvas.drawRGBBitmap(11+64+14+64+14, 14, epd_bitmap_gronkh, 64, 64);
+  canvas.drawRGBBitmap(11+64+14, 14, epd_bitmap_bonjwa, 64, 64);
+  tft.drawRGBBitmap(0, 0, canvas.getBuffer(), canvas.width(), canvas.height());
+  }
+  delay(20);
+}
+
+void setHighFR(){
+  uint8_t params[] = {0x01};
+  tft.sendCommand(0xC6, params, 1);
+}
+
+void set60FR(){
+  uint8_t params[] = {0x0F};
+  tft.sendCommand(0xC6, params, 1);
+}
+
+void enterPartialMode(){
+  uint8_t params[] = {0,50,0,50};
+  tft.sendCommand(ST77XX_PTLAR, params, 4);
+  tft.sendCommand(ST77XX_PTLON, params, 0);
+  delay(10);
+}
+
+void enterNormalMode(){
+  tft.sendCommand(ST77XX_NORON);
+  delay(10);
+}
+
+/*!
+   @brief   Draw a RAM-resident 8-bit image (grayscale) at the specified (x,y)
+   pos. But only the section specified by (o_x, o_y, w, h). Specifically for
+   8-bit display devices such as IS31FL3731; no color reduction/expansion is performed.
+    @param    x   Top left corner x coordinate
+    @param    y   Top left corner y coordinate
+    @param    bitmap  byte array with grayscale bitmap
+    @param    o_x  x offset of section in bitmap
+    @param    o_y  y offset of section in bitmap
+    @param    w   Width of section in pixels
+    @param    h   Height of section in pixels
+    @param    b_w  width of bitmap in pixels
+*/
+/**************************************************************************/
+void drawGrayscaleBitmapSection(int16_t x, int16_t y, uint8_t *bitmap,
+                                       int16_t o_x, int16_t o_y, int16_t w, int16_t h, int16_t b_w) {
+  tft.startWrite();
+  y += o_y;
+  for (int16_t j = o_y; j < (o_y+h); j++, y++) {
+    for (int16_t i = o_x; i < (o_x+w); i++) {
+      tft.writePixel(x + i - o_x, y - o_y, bitmap[j * b_w + i]);
+      //delayMicroseconds(1);
+    }
+  }
+  tft.endWrite();
+}
+
+void drawRGBBitmapSectionFast(int16_t x, int16_t y, uint16_t *bitmap,
+                                       int16_t o_x, int16_t o_y, int16_t w, int16_t h, int16_t b_w) {
+  tft.startWrite();
+  tft.setAddrWindow(x, y, w, h);
+  y += o_y;
+  for (int16_t j = o_y; j < (o_y+h); j++, y++) {
+      tft.writePixels(&bitmap[j * b_w + o_x], w);
+  }
+  tft.endWrite();
+}
+
+void testlines(uint16_t color) {
+  tft.fillScreen(ST77XX_BLACK);
+  for (int16_t x=0; x < tft.width(); x+=6) {
+    tft.drawLine(0, 0, x, tft.height()-1, color);
+    delay(0);
+  }
+  for (int16_t y=0; y < tft.height(); y+=6) {
+    tft.drawLine(0, 0, tft.width()-1, y, color);
+    delay(0);
+  }
+
+  tft.fillScreen(ST77XX_BLACK);
+  for (int16_t x=0; x < tft.width(); x+=6) {
+    tft.drawLine(tft.width()-1, 0, x, tft.height()-1, color);
+    delay(0);
+  }
+  for (int16_t y=0; y < tft.height(); y+=6) {
+    tft.drawLine(tft.width()-1, 0, 0, y, color);
+    delay(0);
+  }
+
+  tft.fillScreen(ST77XX_BLACK);
+  for (int16_t x=0; x < tft.width(); x+=6) {
+    tft.drawLine(0, tft.height()-1, x, 0, color);
+    delay(0);
+  }
+  for (int16_t y=0; y < tft.height(); y+=6) {
+    tft.drawLine(0, tft.height()-1, tft.width()-1, y, color);
+    delay(0);
+  }
+
+  tft.fillScreen(ST77XX_BLACK);
+  for (int16_t x=0; x < tft.width(); x+=6) {
+    tft.drawLine(tft.width()-1, tft.height()-1, x, 0, color);
+    delay(0);
+  }
+  for (int16_t y=0; y < tft.height(); y+=6) {
+    tft.drawLine(tft.width()-1, tft.height()-1, 0, y, color);
+    delay(0);
+  }
+}
+
+void testdrawtext(char *text, uint16_t color) {
+  tft.setCursor(0, 0);
+  tft.setTextColor(color);
+  tft.setTextWrap(true);
+  tft.print(text);
+}
+
+void testfastlines(uint16_t color1, uint16_t color2) {
+  tft.fillScreen(ST77XX_BLACK);
+  for (int16_t y=0; y < tft.height(); y+=5) {
+    tft.drawFastHLine(0, y, tft.width(), color1);
+  }
+  for (int16_t x=0; x < tft.width(); x+=5) {
+    tft.drawFastVLine(x, 0, tft.height(), color2);
+  }
+}
+
+void testdrawrects(uint16_t color) {
+  tft.fillScreen(ST77XX_BLACK);
+  for (int16_t x=0; x < tft.width(); x+=6) {
+    tft.drawRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color);
+  }
+}
+
+void testfillrects(uint16_t color1, uint16_t color2) {
+  tft.fillScreen(ST77XX_BLACK);
+  for (int16_t x=tft.width()-1; x > 6; x-=6) {
+    tft.fillRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color1);
+    tft.drawRect(tft.width()/2 -x/2, tft.height()/2 -x/2 , x, x, color2);
+  }
+}
+
+void testfillcircles(uint8_t radius, uint16_t color) {
+  for (int16_t x=radius; x < tft.width(); x+=radius*2) {
+    for (int16_t y=radius; y < tft.height(); y+=radius*2) {
+      tft.fillCircle(x, y, radius, color);
+    }
+  }
+}
+
+void testdrawcircles(uint8_t radius, uint16_t color) {
+  for (int16_t x=0; x < tft.width()+radius; x+=radius*2) {
+    for (int16_t y=0; y < tft.height()+radius; y+=radius*2) {
+      tft.drawCircle(x, y, radius, color);
+    }
+  }
+}
+
+void testtriangles() {
+  tft.fillScreen(ST77XX_BLACK);
+  uint16_t color = 0xF800;
+  int t;
+  int w = tft.width()/2;
+  int x = tft.height()-1;
+  int y = 0;
+  int z = tft.width();
+  for(t = 0 ; t <= 15; t++) {
+    tft.drawTriangle(w, y, y, x, z, x, color);
+    x-=4;
+    y+=4;
+    z-=4;
+    color+=100;
+  }
+}
+
+void testroundrects() {
+  tft.fillScreen(ST77XX_BLACK);
+  uint16_t color = 100;
+  int i;
+  int t;
+  for(t = 0 ; t <= 4; t+=1) {
+    int x = 0;
+    int y = 0;
+    int w = tft.width()-2;
+    int h = tft.height()-2;
+    for(i = 0 ; i <= 16; i+=1) {
+      tft.drawRoundRect(x, y, w, h, 5, color);
+      x+=2;
+      y+=3;
+      w-=4;
+      h-=6;
+      color+=1100;
+    }
+    color+=100;
+  }
+}
+
+void tftPrintTest() {
+  tft.setTextWrap(false);
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setCursor(0, 30);
+  tft.setTextColor(ST77XX_RED);
+  tft.setTextSize(1);
+  tft.println("Hello World!");
+  tft.setTextColor(ST77XX_YELLOW);
+  tft.setTextSize(2);
+  tft.println("Hello World!");
+  tft.setTextColor(ST77XX_GREEN);
+  tft.setTextSize(3);
+  tft.println("Hello World!");
+  tft.setTextColor(ST77XX_BLUE);
+  tft.setTextSize(4);
+  tft.print(1234.567);
+  delay(1500);
+  tft.setCursor(0, 0);
+  tft.fillScreen(ST77XX_BLACK);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.setTextSize(0);
+  tft.println("Hello World!");
+  tft.setTextSize(1);
+  tft.setTextColor(ST77XX_GREEN);
+  tft.print(p, 6);
+  tft.println(" Want pi?");
+  tft.println(" ");
+  tft.print(8675309, HEX); // print 8,675,309 out in HEX!
+  tft.println(" Print HEX!");
+  tft.println(" ");
+  tft.setTextColor(ST77XX_WHITE);
+  tft.println("Sketch has been");
+  tft.println("running for: ");
+  tft.setTextColor(ST77XX_MAGENTA);
+  tft.print(millis() / 1000);
+  tft.setTextColor(ST77XX_WHITE);
+  tft.print(" seconds.");
+}
+
+void mediabuttons() {
+  // play
+  tft.fillScreen(ST77XX_BLACK);
+  tft.fillRoundRect(25, 10, 78, 60, 8, ST77XX_WHITE);
+  tft.fillTriangle(42, 20, 42, 60, 90, 40, ST77XX_RED);
+  delay(500);
+  // pause
+  tft.fillRoundRect(25, 90, 78, 60, 8, ST77XX_WHITE);
+  tft.fillRoundRect(39, 98, 20, 45, 5, ST77XX_GREEN);
+  tft.fillRoundRect(69, 98, 20, 45, 5, ST77XX_GREEN);
+  delay(500);
+  // play color
+  tft.fillTriangle(42, 20, 42, 60, 90, 40, ST77XX_BLUE);
+  delay(50);
+  // pause color
+  tft.fillRoundRect(39, 98, 20, 45, 5, ST77XX_RED);
+  tft.fillRoundRect(69, 98, 20, 45, 5, ST77XX_RED);
+  // play color
+  tft.fillTriangle(42, 20, 42, 60, 90, 40, ST77XX_GREEN);
+}
